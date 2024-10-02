8eb4d8fead461cdde3fe55f3a056b829aa2c1464
sam-honigblum <sam.honigblum@gmail.com>
2024-10-02 00:06:13 -0400

    login model / authentication reciever

--- users/views.py
+++ users/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
 
 # Create your views here.
+import os
+
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from google.oauth2 import id_token
+from google.auth.transport import requests
+
+
+@csrf_exempt
+def sign_in(request):
+    return render(request, 'sign_in.html')
+
+
+@csrf_exempt
+def auth_receiver(request):
+
+    token = request.POST['credential']
+
+    try:
+        user_data = id_token.verify_oauth2_token(
+            token, requests.Request(), os.environ['GOOGLE_OAUTH_CLIENT_ID']
+        )
+    except ValueError:
+        return HttpResponse(status=403)
+
+    # SAVE USER TO DATABASE HERE 
+
+    request.session['user_data'] = user_data
+
+    return redirect('sign_in')
+
+
