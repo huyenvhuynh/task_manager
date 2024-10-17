bdfcc775fdf191f7fd28361961df971df148a6f2
sam-honigblum <sam.honigblum@gmail.com>
2024-10-16 23:43:25 -0400

    working file upload to s3

--- myapp/models.py
+++ myapp/models.py
@@ -1,13 +1,16 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.core.validators import FileExtensionValidator
+from .storages import AssignmentStorage
 
+# this model is used to store the assignments that the user creates
 class Assignment(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
     due_date = models.DateField()
     file_upload = models.FileField(
-        upload_to='assignments/',
+        upload_to='',
+        storage=AssignmentStorage(),
         validators=[FileExtensionValidator(['txt', 'pdf', 'jpg'])],
         null=True,
         blank=True
