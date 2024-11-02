20759e42efbcd09d0d11fcb8deed01700d61cd64
Your Name <imc6x@virginia.edu>
2024-11-02 15:57:02 -0400

    Set up form for course creation in a new model

--- users/models.py
+++ users/models.py
@@ -9,6 +9,7 @@ class Profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     role = models.CharField(max_length=10, choices=ROLE_CHOICES, default='user')
     image = models.ImageField(upload_to='jpg/', default='default.jpg')
+    
 
     def __str__(self):
         return f"{self.user.email} - {self.role}"
