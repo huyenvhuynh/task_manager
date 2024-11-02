7a3ba0926197fe50e0cd2b56965962f9ab8cc311
Your Name <imc6x@virginia.edu>
2024-11-02 16:10:52 -0400

    Added courses attribute to profile model

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import Group
+from .forms import CourseGroupForm
 
-# Create your views here.
+
+def create_course_group(request):
+    if request.method == 'POST':
+        form = CourseGroupForm(request.POST)
+        if form.is_valid():
+            course = form.save()
+            group = Group.objects.create(name=course.full_name)
+            request.user.groups.add(group)
+            return redirect('course_detail', pk=course.pk)
+    else:
+        form = CourseGroupForm()
+    
+    return render(request, 'courses/create_course.html', {'form': form})
